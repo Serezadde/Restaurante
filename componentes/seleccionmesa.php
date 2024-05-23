ba845f3c69c213639956a3169eccdebe7119526e
moisesPC <alu0101204239@ull.edu.es>
2024-05-23 20:16:55 +0100

    Cambios

--- componentes/seleccionmesa.php
+++ componentes/seleccionmesa.php
@@ -1,14 +1,23 @@
-<div class="form-group">
-    <label for="seleccionMesa">Mesas</label>
-    <select class="form-select" name="seleccionMesa" id="seleccionMesa" aria-label="Seleccionar Mesa" style="font-size: 18px;">        <?php
-        include "../../../modelo/conexion.php";
-        $sql = "CALL ObtenerMesasDisponibles()";
-        $result = $conexion->query($sql);
-        if ($result->num_rows > 0) {
-            while ($categoria = $result->fetch_assoc()) {
-                echo "<option value='" . $categoria['id'] . "'>" . $categoria['nombre'] . "</option>";
-            }
-        }
-        ?>
-    </select>
-</div>
+<?php
+include "../../../modelo/conexion.php";
+$sql = "CALL ObtenerMesasDisponibles()";
+$result = $conexion->query($sql);
+$mesas = [];
+
+if ($result && $result->num_rows > 0) {
+    while ($mesas_fetch = $result->fetch_assoc()) {
+        $mesas[] = $mesas_fetch;
+    }
+}
+if (!empty($mesas)) {
+    //ponemos estas label aqui para que se visualice correctamente luego
+    echo '<div class="form-group">';
+    echo '<label for="seleccionMesa">Mesas</label>';
+    echo '<select class="form-select" name="seleccionMesa" id="seleccionMesa" aria-label="Seleccionar Mesa" style="font-size: 18px;">';
+    foreach ($mesas as $mesa) {
+        echo "<option value='" . $mesa['id'] . "'>" . $mesa['nombre'] . "</option>";
+    }
+    echo '</select>';
+    echo '</div>';
+}
+?>
