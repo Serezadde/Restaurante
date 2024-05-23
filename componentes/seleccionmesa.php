35ed743595d67b9191e36c52046de0f3552fb1b0
moisesPC <alu0101204239@ull.edu.es>
2024-05-23 19:26:33 +0100

    Cambio

--- componentes/seleccionmesa.php
+++ componentes/seleccionmesa.php
@@ -2,7 +2,7 @@
     <label for="seleccionMesa">Mesas</label>
     <select class="form-select" name="seleccionMesa" id="seleccionMesa" aria-label="Seleccionar Mesa" style="font-size: 18px;">        <?php
         include "../../../modelo/conexion.php";
-        $sql = "SELECT * FROM mesa";
+        $sql = "CALL ObtenerMesasDisponibles()";
         $result = $conexion->query($sql);
         if ($result->num_rows > 0) {
             while ($categoria = $result->fetch_assoc()) {
